81fd6523245f1ee906e217454c406c46c013836c
UltimateAnsh <137130278+UltimateAnsh@users.noreply.github.com>
2024-04-08 18:58:41 +0530

    Update init.device.sh

--- recovery/root/system/bin/init.device.sh
+++ recovery/root/system/bin/init.device.sh
@@ -6,14 +6,14 @@ echo $prjName
 
 case $prjName in
     "20682")
-        resetprop "ro.product.model" "Realme 7/ Narzo 20 Pro "
-        resetprop "ro.build.product" "RMX2151"
-        resetprop "ro.product.device" "RMX2151"
+        resetprop "ro.product.model" "Realme 7/Narzo 20 Pro/Narzo 30 4G "
+        resetprop "ro.build.product" "RM6785"
+        resetprop "ro.product.device" "RM6785"
         ;;
     *)
-        resetprop "ro.product.model" "Realme 6"
-        resetprop "ro.build.product" "RMX2001"
-        resetprop "ro.product.device" "RMX2001"
+        resetprop "ro.product.model" "Realme 6/6i(Indian)/6s/Narzo"
+        resetprop "ro.build.product" "RM6785"
+        resetprop "ro.product.device" "RM6785"
         ;;
 esac
 
